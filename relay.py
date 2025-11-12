f86ca388d07dd67ca5f399adc4ab3c430c4daa10
NickSpv <91346886+NickSpv@users.noreply.github.com>
2025-11-12 14:07:22 +0300

    Update relay.py

--- relay.py
+++ relay.py
@@ -11,7 +11,7 @@ socks_server_reply_success = b"\x00\x5a\x00\x00\x00\x00\x00\x00"
 socks_server_reply_fail = b"\x00\x5b\x00\x00\x00\x00\x00\x00"
 COMMAND_CHANNEL = 0
 
-# Changed from string characters to integer values
+# Оставить текущие значения, но исправить cmd_names
 CHANNEL_CLOSE_CMD = 0xCC
 CHANNEL_OPEN_CMD = 0xDD
 FORWARD_CONNECTION_SUCCESS = 0xEE
@@ -19,14 +19,14 @@ FORWARD_CONNECTION_FAILURE = 0xFF
 CLOSE_RELAY = 0xC4
 PING_CMD = 0x70
 
-# Updated cmd_names to use integer keys
+# ИСПРАВИТЬ ТОЛЬКО ЭТО - использовать байты вместо целых чисел
 cmd_names = {
-    CHANNEL_CLOSE_CMD: 'CHANNEL_CLOSE_CMD',
-    CHANNEL_OPEN_CMD: 'CHANNEL_OPEN_CMD',
-    FORWARD_CONNECTION_SUCCESS: 'FORWARD_CONNECTION_SUCCESS',
-    FORWARD_CONNECTION_FAILURE: 'FORWARD_CONNECTION_FAILURE',
-    CLOSE_RELAY: 'CLOSE_RELAY',
-    PING_CMD: 'PING_CMD'
+    b'\xCC': 'CHANNEL_CLOSE_CMD',
+    b'\xDD': 'CHANNEL_OPEN_CMD', 
+    b'\xEE': 'FORWARD_CONNECTION_SUCCESS',
+    b'\xFF': 'FORWARD_CONNECTION_FAILURE',
+    b'\xC4': 'CLOSE_RELAY',
+    b'\x70': 'PING_CMD'
 }
 
 
